93e79f3f859007f0a55745725bb164d5eec96a5c
Md Nuruddin Osman <mdnuruddin378350@gmail.com>
2024-03-03 01:19:16 +0600

    Footer done

--- src/components/utilities/HeadingH5.jsx
+++ src/components/utilities/HeadingH5.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const HeadingH5 = ({content, className}) => {
   return (
     <>
-        <h5 className={`text-[23px] font-inter font-extrabold leading-[30px] text-[#FFFFFF] ${className}`}>{content}</h5>
+        <h5 className={`text-[23px] font-inter font-extrabold leading-[30px] ${className}`}>{content}</h5>
     </>
   )
 }
--- src/section/footer/Footer.jsx
+++ src/section/footer/Footer.jsx
@@ -3,42 +3,68 @@ import { FaFacebookF } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaYoutube } from "react-icons/fa";
 import HeadingH5 from '../../components/utilities/HeadingH5';
+import Path from '../../assets/image/path.png'
 
 const Footer = () => {
   return (
     <section className='bg-[#02033B]'>
         <div className="max-w-container mx-auto py-24">
-            <div className="">
-                <HeadingH5 className='text-[#FFF]' content="Scan. Detect. Remove."/>
-                <div className="flex gap-10 my-12">
-                    <div className="h-9 w-9 flex items-center justify-center bg-[#FFFFFF] rounded-[5px]">
-                        <FaFacebookF className='text-2xl' />
+            <div className="flex justify-between">
+                <div className="">
+                    <HeadingH5 className='text-[#FFF]' content="Scan. Detect. Remove."/>
+                    <div className="flex gap-10 my-12">
+                        <div className="h-9 w-9 flex items-center justify-center bg-[#FFFFFF] rounded-[5px]">
+                            <FaFacebookF className='text-2xl' />
+                        </div>
+                        <div className="h-9 w-9 flex items-center justify-center bg-[#FFFFFF] rounded-[5px]">
+                            <FaTwitter className='text-2xl' />
+                        </div>
+                        <div className="h-9 w-9 flex items-center justify-center bg-[#FFFFFF] rounded-[5px]">
+                            <FaYoutube className='text-2xl' />
+                        </div>
                     </div>
-                    <div className="h-9 w-9 flex items-center justify-center bg-[#FFFFFF] rounded-[5px]">
-                        <FaTwitter className='text-2xl' />
+                    <div className="flex gap-[26px]">
+                        <a className='content11px underline' href="#">Privacy Policy</a>
+                        <a className='content11px underline' href="#">Terms of Service</a>
                     </div>
-                    <div className="h-9 w-9 flex items-center justify-center bg-[#FFFFFF] rounded-[5px]">
-                        <FaYoutube className='text-2xl' />
+                    <div className="mt-12">
+                        <article className='content11px w-[312px]'>Copyright © 2022 Certo Software Limited | Registered in England & Wales No. 10072356</article>
+                    </div>
+                    <div className="mt-5">
+                        <span className='content11px'>Designed & developed by <a className='content11px underline' href="#">Bigger Picture</a></span>
                     </div>
                 </div>
-                <div className="flex gap-[26px]">
-                    <a className='content11px underline' href="#">Privacy Policy</a>
-                    <a className='content11px underline' href="#">Terms of Service</a>
+                <div className="">
+                    <div className="">
+                        <h3 className='text-[35px] font-inter font-extrabold leading-[45px] text-[#FFFFFF]'>Miro.</h3>
+                        <hr className='w-[178px] mt-[25px] mb-[60px]' />
+                    </div>
+                    <ul className='flex flex-col gap-6'>
+                        {/* ============index.css => (device) common class============= */}
+                        <li className='device'>iPhone</li>
+                        <li className='device'>Android</li>
+                        <li className='device'>Help</li>
+                        <li className='device'>About</li>
+                        <li className='device'>Insights</li>
+                    </ul>
                 </div>
-                <div className="mt-12">
-                    <span className='content11px'>Copyright © 2022 Certo Software Limited | Registered in England & Wales No. 10072356</span>
-                </div>
-                <div className="mt-5">
-                    <span className='content11px'>Designed & developed by <a className='content11px underline' href="#">Bigger Picture</a></span>
+                <div className="bg-[rgb(253,146,53)] relative w-[436px] h-[232px] rounded-[42px]">
+                    <div className="h-full w-[190px] absolute top-0 left-1/2 -translate-x-1/2 z-10">
+                        <img className="h-full w-full" src={Path} alt="Path" />
+                    </div>
+                    <div className="absolute top-0 left-0 p-9 z-20">
+                        <HeadingH5 content="Sign up to our newsletter" className="text-[#02033B]"/>
+                        <article className='text-[15px] font-inter font-normal leading-5 text-[#02033B] mt-[21px] mb-9'>Receive the latest mobile security news, exclusive discounts & offers straight to your inbox!</article>
+                        <div className="w-full h-ful">
+                            <div className="w-[364px] h-[35px] relative rounded-3xl overflow-hidden">
+                                <input type="text" placeholder='Email address' className='text-[15px] font-inter font-normal text-[#02033B] py-2 pl-4 pr-[130px] w-full h-full outline-none'/>
+                                <button className='text-[15px] font-inter font-extrabold leading-5 text-[#FFFFFF] absolute top-0 right-0 bg-[#02033B] h-full px-7'>Submit</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div className="">
-                <h3 className='text-[35px] font-inter font-extrabold leading-[45px] text-[#FFFFFF]'>Miro.</h3>
-                <hr className='w-[178px] mt-[25px]' />
-            </div>
-            <ul>
-                <li className='device'>Android</li>
-            </ul>
+                <article className='text-[12px] font-inter font-normal leading-[15px] text-[#FFFFFF] w-[550px] mt-[80px]'>Apple, the Apple logo, and iPhone are trademarks of Apple Inc., registered in the U.S. and other countries. App Store is a service mark of Apple Inc. Android, Google Play and the Google Play logo are trademarks of Google LLC.</article>
         </div>
     </section>
   )
